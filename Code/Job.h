89fd4a8270e5ec80444d4e7dea137f3aabab03fb
Kendall Boesch <kboesch@smu.edu>
2023-09-28 12:31:09 -0500

    Job.h complete

---
+++ Code/Job.h
@@ -0,0 +1,23 @@
+#pragma once 
+
+class Job 
+{
+    friend class JobSystem; 
+    friend class JobWorkerThread;
+    public: 
+        Job(unsigned long jobChannels = 0xFFFFFFFF, int jobType = -1) : m_jobChannels(jobChannels), m_jobType(jobType)
+        {
+            static int s_nextJobID = 0; 
+            m_jobID = s_nextJobID; 
+        }
+        virtual ~Job() {}
+        virtual void execute() = 0; 
+        virtual void jobCompleteCallback() {}
+        int getUniqueID() const {return m_jobID;}
+
+    private: 
+        int m_jobID = -1; 
+        int m_jobType = -1; 
+
+        unsigned long m_jobChannels = 0xFFFFFFFF; 
+}

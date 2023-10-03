f0afa714e4a76e2087e5398f10316023c8dbfdcc
Kendall Boesch <kboesch@smu.edu>
2023-10-03 15:55:23 -0500

    basics of JSONJob.h

--- Code/JSONJob.h
+++ Code/JSONJob.h
@@ -1,9 +1,16 @@
 #pragma once 
 #include "Job.h"
+#include "ParseJob.h"
+
 
 
 class JSONJob : public Job
 {
-    
+    JSONJob(unsigned long jobChannel = 0xFFFFFFFF, int jobType = -1) : Job(jobChannel, jobType) {}; 
+    ~JSONJob(){}; 
+    void execute() override; 
+
+    std::string filePath; 
+    std::vector<Error> errors; 
 
 }; 
